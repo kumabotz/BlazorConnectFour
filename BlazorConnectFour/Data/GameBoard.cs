e8a38b7b4545c8f27fbc47d6407e5c6b280ddbad
Tery Lim <kumabotz@gmail.com>
2019-11-07 00:00:13 +1300

    Model the game board, game piece and turn

---
+++ BlazorConnectFour/Data/GameBoard.cs
@@ -0,0 +1,27 @@
+﻿namespace BlazorConnectFour.Data
+{
+    public class GameBoard
+    {
+        /// 7 cols x 6 rows, [col,row] starts from top left
+        /// [0,0] .. [6,0]
+        /// ..
+        /// [0,5] .. [6,5]
+        public GamePiece[,] Board { get; set; }
+
+        public const int Cols = 7;
+        public const int Rows = 6;
+
+        public GameBoard()
+        {
+            Board = new GamePiece[Cols, Rows];
+
+            for (var i = 0; i < Cols; i++)
+            {
+                for (var j = 0; j < Rows; j++)
+                {
+                    Board[i, j] = new GamePiece(PieceColor.Blank);
+                }
+            }
+        }
+    }
+}
---
+++ BlazorConnectFour/Data/Turn.cs
@@ -0,0 +1,32 @@
+﻿namespace BlazorConnectFour.Data
+{
+    public class Turn
+    {
+        public PieceColor Color { get; }
+        public string Emoji { get; }
+        public Position Position { get; private set; }
+
+        public Turn(PieceColor color, string emoji)
+        {
+            Color = color;
+            Emoji = emoji;
+        }
+
+        public void SetPosition(Position position)
+        {
+            Position = position;
+        }
+    }
+
+    public struct Position
+    {
+        public int X;
+        public int Y;
+
+        public Position(int x = -1, int y = -1)
+        {
+            X = x;
+            Y = y;
+        }
+    }
+}
