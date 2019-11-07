68f25df0a00f06c7a347359d23a3acd533820976
Tery Lim <kumabotz@gmail.com>
2019-11-07 21:50:03 +1300

    Playable but no game rules yet

--- BlazorConnectFour/Data/Turn.cs
+++ BlazorConnectFour/Data/Turn.cs
@@ -4,29 +4,11 @@
     {
         public PieceColor Color { get; }
         public string Emoji { get; }
-        public Position Position { get; private set; }
 
         public Turn(PieceColor color, string emoji)
         {
             Color = color;
             Emoji = emoji;
-        }
-
-        public void SetPosition(Position position)
-        {
-            Position = position;
-        }
-    }
-
-    public struct Position
-    {
-        public int X;
-        public int Y;
-
-        public Position(int x = -1, int y = -1)
-        {
-            X = x;
-            Y = y;
         }
     }
 }
